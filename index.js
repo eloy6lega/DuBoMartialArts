5186516e09b8336c454602bcb6db17a4f3920966
eloy6lega <eloy.perez@campusfp.es>
2024-01-14 17:07:11 +0100

    DuBoMartialArts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const https = require("https");
 const fs = require("fs");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const DB = require("./db.json");
 
 const options = {
